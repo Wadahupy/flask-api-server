63dab17ff357c701f168749d804a251763bc7be4
Denzkieeeeee <116092662+Wadahupy@users.noreply.github.com>
2024-12-17 00:50:02 +0800

    initial

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -3,24 +3,32 @@ from server.controllers.emotion_detection import predict_text_emotion, predict_a
 
 prediction = Blueprint('predict', __name__)
 
-@prediction.route('/predict', methods=['POST'])
-def predict_emotion():
-    if 'text' in request.json:
-        text = request.json['text']
-        predicted_emotion, confidence = predict_text_emotion(text)
-        return jsonify({"type": "text", "predicted_emotion": predicted_emotion, "confidence": confidence})
+@prediction.route('/text', methods=['POST'])
+def text_prediction():
 
-    if 'file' in request.files:
-        file = request.files['file']
-        file_path = "temp_audio.wav"
-        file.save(file_path)
+    text = request.json['text']
 
-        predicted_emotion, confidence = predict_audio_emotion(file_path)
-        cleanup_file(file_path)
+    predicted_emotion, confidence = predict_text_emotion(text)
 
-        if confidence.get("error"):
-            return jsonify({"error": confidence["error"]}), 500
+    return jsonify({"type": "text", "predicted_emotion": predicted_emotion, "confidence": confidence})
 
-        return jsonify({"type": "audio", "predicted_emotion": predicted_emotion, "confidence": confidence})
+@prediction.route('/audio', methods=['POST'])
+def audio_prediction():
 
-    return jsonify({"error": "Invalid request. Provide either 'text' or 'file'."}), 400
+    file = request.files['file']
+
+    # Validate file type
+    if not file.mimetype.startswith('audio/'):
+        return jsonify({"error": "Unsupported file type. Please upload an audio file."}), 400
+
+    file_path = "temp_audio.wav"
+    file.save(file_path)
+    print(f"Saved file for debugging at: {file_path}")
+
+    predicted_emotion, confidence = predict_audio_emotion(file_path)
+    cleanup_file(file_path)
+
+    if confidence.get("error"):
+        return jsonify({"error": confidence["error"]}), 500
+
+    return jsonify({"type": "audio", "predicted_emotion": predicted_emotion, "confidence": confidence})
